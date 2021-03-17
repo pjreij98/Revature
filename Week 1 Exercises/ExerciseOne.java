8bc9c8ad45887c04b87a1a0a7bbbc9030d77aabf
pjreij98 <70818877+pjreij98@users.noreply.github.com>
2021-03-17 12:54:01 -0500

    Add files via upload

---
+++ Week 1 Exercises/ExerciseOne.java
@@ -0,0 +1,8 @@
+public class ExerciseOne {
+    public static void main(String[] args)
+    {
+        double x = 10.5;
+        double y = 11.5;
+        System.out.println(x * y);
+    }
+}
---
+++ Week 1 Exercises/ExerciseTwo.java
@@ -0,0 +1,14 @@
+public class ExerciseTwo {
+    public static void main(String[] args)
+    {
+        int x = 45;
+        if(x == 3)
+        {
+            System.out.println("x is equal to 3");
+        }
+        else
+        {
+            System.out.println("x is NOT equal to 3");
+        }
+    }
+}
